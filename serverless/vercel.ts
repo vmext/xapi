af1cc3f4cae6d9e4b99a82ce0a6f341713269434
sd <leowest1921@gmail.com>
2025-08-08 15:42:02 +0800

    add

--- serverless/vercel.ts
+++ serverless/vercel.ts
@@ -1,8 +1,5 @@
-//import serverless from "serverless-http"
+import serverless from "serverless-http";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../src/app";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  app.callback()(req, res);
-}
-//export default serverless(app);
+export const handler = serverless(app);
