b135763527f799ddbb393fa9c0f4adcbb9903fc0
sd <leowest1921@gmail.com>
2025-08-10 09:49:03 +0800

    add

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import Koa from "koa";
 import api from "./api";
+import cors from "@koa/cors";
 
 const app = new Koa();
+// 允许跨域
+app.use(
+  cors({
+    origin(ctx) {
+      return ctx.get("Origin") || "*";
+    },
+  })
+);
 
 app.use(api.routes()).use(api.allowedMethods());
 export default app;
