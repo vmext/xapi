2bd46718c3c3017a812371155d25fabb081f6e82
sd <leowest1921@gmail.com>
2025-08-10 10:06:40 +0800

    add

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,7 @@ import cors from "@koa/cors";
 
 const app = new Koa();
 // 允许跨域
-app.use(async (ctx, next) => {
-  ctx.set("Access-Control-Allow-Origin", "*");
-  ctx.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Content-Length, Authorization, Accept, X-Requested-With"
-  );
-  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  if (ctx.method == "OPTIONS") {
-    ctx.body = "";
-  } else {
-    await next();
-  }
-});
+app.use(cors());
 
 app.use(api.routes()).use(api.allowedMethods());
 export default app;
