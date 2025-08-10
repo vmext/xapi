f1b500a779aa4900dd8d3f3bbe18b7fc28a110ac
sd <leowest1921@gmail.com>
2025-08-10 10:15:59 +0800

    add

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,13 @@ import cors from "@koa/cors";
 
 const app = new Koa();
 // 允许跨域
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // 允许所有域名跨域请求，如果你只想允许某几个域名，请写对应的域名字符串或者函数
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization", "Accept"],
+  })
+);
 
 app.use(api.routes()).use(api.allowedMethods());
 export default app;
